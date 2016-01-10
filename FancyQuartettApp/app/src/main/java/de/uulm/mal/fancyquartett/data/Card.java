e6a41e7f4c78f458d88a5be48966047a5934188b
Ferdi B <ferdinand.birk@uni-ulm.de>
2016-01-10 20:58:44 +0100

    Added sort function to AttributeList in order to have a deterministic Order in which the Attributes are shown on the CardView

--- FancyQuartettApp/app/src/main/java/de/uulm/mal/fancyquartett/data/Card.java
+++ FancyQuartettApp/app/src/main/java/de/uulm/mal/fancyquartett/data/Card.java
@@ -5,6 +5,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,26 +31,26 @@ public class Card {
 
 
     // constructor used by OnlineDecks when downloading
-    public Card(JSONObject json, Property[] props, String pathToOnlinePictures,String localFolder,String deckname,  boolean isLocal) throws JSONException {
+    public Card(JSONObject json, Property[] props, String pathToOnlinePictures, String localFolder, String deckname, boolean isLocal) throws JSONException {
         images = new ArrayList<Image>();
-        values= new HashMap<>();
+        values = new HashMap<>();
         this.id = json.getInt("id");
         this.name = json.getString("name");
         this.description = json.getString("description");
-        this.deckname=deckname;
+        this.deckname = deckname;
         JSONArray imgsJson = json.getJSONArray("images");
-        for(int i = 0; i < imgsJson.length(); i++) {
-            Image img = new Image(imgsJson.getJSONObject(i), pathToOnlinePictures,localFolder,deckname, isLocal);
-            if(!isLocal) img.download();
+        for (int i = 0; i < imgsJson.length(); i++) {
+            Image img = new Image(imgsJson.getJSONObject(i), pathToOnlinePictures, localFolder, deckname, isLocal);
+            if (!isLocal) img.download();
             images.add(img);
         }
         JSONArray valsJson = json.getJSONArray("values");
-        for(int i = 0; i < valsJson.length(); i++) {
+        for (int i = 0; i < valsJson.length(); i++) {
             JSONObject pair = valsJson.getJSONObject(i);
             int propId = pair.getInt("propertyId");
             float val = (float) pair.getDouble("value");
-            for(int j = 0; j < props.length; j++) { // search property with given id for each value
-                if(props[j].id()==propId) {
+            for (int j = 0; j < props.length; j++) { // search property with given id for each value
+                if (props[j].id() == propId) {
                     values.put(props[j], val);
                     break;
                 }
@@ -56,26 +59,26 @@ public class Card {
     }
 
     // constructor used by OfflineDecks
-    public Card(JSONObject json, ArrayList<Property> props, String localFolder,String deckname,  boolean isLocal) throws JSONException {
+    public Card(JSONObject json, ArrayList<Property> props, String localFolder, String deckname, boolean isLocal) throws JSONException {
         images = new ArrayList<Image>();
-        values= new HashMap<>();
+        values = new HashMap<>();
         this.id = json.getInt("id");
         this.name = json.getString("name");
         this.description = json.getString("description");
-        this.deckname=deckname;
+        this.deckname = deckname;
         JSONArray imgsJson = json.getJSONArray("images");
-        for(int i = 0; i < imgsJson.length(); i++) {
-            Image img = new Image(imgsJson.getJSONObject(i), localFolder,deckname, isLocal);
-            if(!isLocal) img.download();
+        for (int i = 0; i < imgsJson.length(); i++) {
+            Image img = new Image(imgsJson.getJSONObject(i), localFolder, deckname, isLocal);
+            if (!isLocal) img.download();
             images.add(img);
         }
         JSONArray valsJson = json.getJSONArray("values");
-        for(int i = 0; i < valsJson.length(); i++) {
+        for (int i = 0; i < valsJson.length(); i++) {
             JSONObject pair = valsJson.getJSONObject(i);
             int propId = pair.getInt("propertyId");
             float val = (float) pair.getDouble("value");
-            for (Property prop:props){
-                if(prop.id()==propId) {
+            for (Property prop : props) {
+                if (prop.id() == propId) {
                     values.put(prop, val);
                     break;
                 }
@@ -92,52 +95,69 @@ public class Card {
         this.images = images;
     }
 
-    public int id() { return id; }
+    public int id() {
+        return id;
+    }
 
     // overloaded add method
     public void add(Image image) {
         images.add(image);
     }
+
     public void add(String description) {
         this.description = description;
     }
 
     // compare with other card's respective property and return true if this card wins
     public boolean compare(Property prop, float enemyValue) {
-        return prop.biggerWins() ? values.get(prop)>enemyValue : values.get(prop)<enemyValue;
+        return prop.biggerWins() ? values.get(prop) > enemyValue : values.get(prop) < enemyValue;
     }
 
     /**
      * This function converts Map<Property,Float> into List<CardAttribute>
+     *
      * @param values
      * @return
      */
     private List<CardAttribute> makeAttrList(Map<Property, Float> values) {
         List<CardAttribute> list = new ArrayList<CardAttribute>();
-        for(Property property : values.keySet()) {
+        for (Property property : values.keySet()) {
             float value = values.get(property);
             list.add(new CardAttribute(property, value));
+            //Sort based on PropertyID
+            Collections.sort(list, new Comparator<CardAttribute>() {
+                        @Override
+                        public int compare(CardAttribute lhs, CardAttribute rhs) {
+                            if (lhs.getProperty().id() < rhs.getProperty().id()) {
+                                return -1;
+                            } else if (lhs.getProperty().id() > rhs.getProperty().id()) {
+                                return 1;
+                            } else {
+                                return 0;
+                            }
+                        }
+                    }
+            );
         }
         return list;
     }
 
-    public List<CardAttribute> getAttributeList(){
+    public List<CardAttribute> getAttributeList() {
         return makeAttrList(values);
     }
 
     /**
-     *
      * @return
      */
+
     public CharSequence getName() {
         return name;
     }
 
     /**
-     *
      * @return
      */
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
 
