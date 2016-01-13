efa792628a887dfb3ac687047a256afd82f9f7e1
Lukas Stöferle <lukas.stoeferle@uni-ulm.de>
2016-01-13 12:40:00 +0100

    Fancy ToTheEndThread
    ToTheEnd-GameMode functionallity (still in progress...!)

--- FancyQuartettApp/app/src/main/java/de/uulm/mal/fancyquartett/activities/GameActivity.java
+++ FancyQuartettApp/app/src/main/java/de/uulm/mal/fancyquartett/activities/GameActivity.java
@@ -3,10 +3,9 @@ package de.uulm.mal.fancyquartett.activities;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
@@ -25,7 +24,7 @@ import de.uulm.mal.fancyquartett.data.Property;
 import de.uulm.mal.fancyquartett.data.Settings;
 import de.uulm.mal.fancyquartett.enums.GameMode;
 import de.uulm.mal.fancyquartett.enums.KILevel;
-import de.uulm.mal.fancyquartett.threads.ToTheEndThread;
+import de.uulm.mal.fancyquartett.tasks.ToTheEndTask;
 import de.uulm.mal.fancyquartett.utils.LocalDeckLoader;
 import layout.CardFragment;
 
@@ -95,27 +94,17 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
      */
     @Override
     public void onDeckLoaded(OfflineDeck offlineDeck) {
-
         // create gameEngine
         engine = new GameEngine(getApplicationContext(), offlineDeck);
         engine.initialiseSingleplayerGame();
         engine.startSingleplayerGame();
     }
 
-
-
-    /**
-     * //TODO @Lukas: Hier kannst du die Engine mit methoden ansteuern...
-     * If a card attribute is clicked, the event is routed to this Method
-     *
-     * @param property
-     * @param value
-     * @param cardAttribute
-     */
     @Override
     public void onCardFragmentAttributeInteraction(Property property, float value, CardAttribute cardAttribute) {
-        //TODO sth stuff with your engine z.B
-        engine.compareCardsProperty(property);
+        if(engine.toTheEndTask != null) {
+            engine.toTheEndTask.onCardAttrClicked(property, value, cardAttribute);
+        }
     }
 
 
@@ -124,14 +113,17 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
      */
     public class GameEngine implements Serializable {
 
-        private final int STINGSTACK = -1;
-        private final int PLAYER1 = 1;
-        private final int PLAYER2 = 2;
-        private final boolean PLAYER1_BEGINS = false;
-        private final boolean PLAYER2_BEGINS = true;
+        public final int STANDOFF = 0;
+        public final int PLAYER1 = 1;
+        public final int PLAYER2 = 2;
+        public final boolean PLAYER1_BEGINS = false;
+        public final boolean PLAYER2_BEGINS = true;
 
         // app attributes
         private final Context context;
+
+        // tasks
+        ToTheEndTask toTheEndTask;
 
         // game attributes
         private final OfflineDeck gameDeck;
@@ -144,8 +136,8 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
         private int maxPoints = 0;
         private int timeout = 0;
         private KILevel kiLevel;
-        private boolean playerBegins = false; // false (player1) , true (player2)
-        private boolean currentPlayer = false; // false (player1) , true (player2)
+        private int playerBegins = 0; // false (player1) , true (player2)
+        private int curPlayer = 0; // false (player1) , true (player2)
         private boolean isMultiplayer = false;
         private List<Card> stingStack;
 
@@ -184,12 +176,19 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
 
 
         /**
-         * Reads all necessary data from GameActivity.
+         * Reads all necessary data from GameActivity for Singleplayer-Mode.
          */
         public void initialiseSingleplayerGame() {
             this.gameMode = bundleGameMode;
             this.kiLevel = bundleKILevel;
-            this.playerBegins = Math.random() < 0.5;
+            this.stingStack = new ArrayList<Card>();
+            if(Math.random() < 0.5) {
+                this.playerBegins = PLAYER1;
+                this.curPlayer = PLAYER1;
+            } else {
+                this.playerBegins = PLAYER2;
+                this.curPlayer = PLAYER2;
+            }
             this.maxRounds = bundleMaxRounds;
             if(gameMode == GameMode.Time) {
                 this.timeout = bundleRoundTimeout;
@@ -203,6 +202,9 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
             initialiseGUI();
         }
 
+        /**
+         * Reads all necessary data from GameActivity for Multiplayer-Mode.
+         */
         public void initialiseMultiplayerGame() {
             // TODO
         }
@@ -217,10 +219,13 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
             spreadCards(cards);
         }
 
+        /**
+         * Initialise all necessary GUI-elements.
+         */
         public void initialiseGUI() {
             // create cardFragment
             cardFragment = CardFragment.newInstance(offlineDeck.getCards().get(0));
-            getSupportFragmentManager().beginTransaction().add(R.id.linLayout_Container, cardFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.linLayout_Container, cardFragment).commit();
             progressBar.setProgress(50);
 
 
@@ -238,8 +243,8 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
             setLastPlayed();
             // start game
             if(gameMode == GameMode.ToTheEnd) {
-                ToTheEndThread thread = new ToTheEndThread(this);
-                thread.start();
+                toTheEndTask = new ToTheEndTask(this.context, this);
+                toTheEndTask.execute();
             }
         }
 
@@ -247,7 +252,19 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
 
         }
 
-        public int checkPlayerWon(GameMode gameMode) {
+        public void changeCurrentPlayer() {
+            if(curPlayer == PLAYER1) {
+                curPlayer = PLAYER2;
+            } else {
+                curPlayer = PLAYER1;
+            }
+        }
+
+        /**
+         * Checks if player won. Returns -1 if no player has yet won.
+         * @return
+         */
+        public int checkPlayerWon() {
             if(gameMode == GameMode.Points) {
                 if(p1Points >= maxPoints) return PLAYER1;
                 if(p2Points >= maxPoints) return PLAYER2;
@@ -261,13 +278,18 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
             return -1;
         }
 
-        public void setLastPlayed() {
-            Date date = new Date();
-            this.lastPlayed = new Timestamp(date.getTime());
-        }
     /*
         GUI - CONTROLLING
     */
+
+        /**
+         * Shows the first card in player1s card-deck.
+         * (Be sure you've done handlePlayerCards(...) before!)
+         */
+        public void showNextCard() {
+            cardFragment = cardFragment.newInstance(p1Cards.get(0));
+            getSupportFragmentManager().beginTransaction().replace(R.id.linLayout_Container, cardFragment).commit();
+        }
 
         // TODO: some button enable/disable stuff, coloring (point-mode), progress bar, textViews (cardQuantity)
 
@@ -319,18 +341,32 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
         }
 
         /**
+         * Removes current card from given players card-deck.
+         * @param player
+         * @return
+         */
+        public Card removeCardFromPlayer(int player) {
+            if(player == PLAYER1) {
+                return p1Cards.remove(0);
+            } else {
+                return p2Cards.remove(0);
+            }
+        }
+
+        /**
          * Removes a given card from given players card-deck.
          * @param card
          * @param player
          */
-        public void removeCardFromPlayer(Card card, int player) {
+        public Card removeCardFromPlayer(Card card, int player) {
             if(isCardOfPlayer(card, player)) {
                 if(player == PLAYER1) {
-                    p1Cards.remove(p1Cards.indexOf(card));
+                    return p1Cards.remove(p1Cards.indexOf(card));
                 } else {
-                    p2Cards.remove(p2Cards.indexOf(card));
+                    return p2Cards.remove(p2Cards.indexOf(card));
                 }
             }
+            return null;
         }
 
         /**
@@ -391,8 +427,37 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
         }
 
         /**
+         * Removes card from loser and adds it to winners card-deck. If stingStack contains some
+         * cards, then they will be addet to to winners card-deck.
+         * @param playerWon
+         */
+        public void handlePlayerCards(int playerWon) {
+            if(playerWon != engine.STANDOFF) {
+                if(playerWon == engine.PLAYER1) {
+                    Card card = engine.removeCardFromPlayer(engine.PLAYER2);
+                    engine.queueCard(playerWon);
+                    engine.addCardToPlayer(card, playerWon);
+                }
+                if(playerWon == engine.PLAYER2) {
+                    Card card = engine.removeCardFromPlayer(engine.PLAYER1);
+                    engine.queueCard(playerWon);
+                    engine.addCardToPlayer(card, playerWon);
+                }
+                if(stingStack.size() > 0) {
+                    System.out.println("Player " + playerWon + "gets " + stingStack.size() + " crads from stingstack");
+                    removeCardsFromStingStag(playerWon);
+
+                }
+            } else {
+                Card p1Card = engine.removeCardFromPlayer(engine.PLAYER1);
+                Card p2Card = engine.removeCardFromPlayer(engine.PLAYER2);
+                engine.addCardsToStingStag(p1Card, p2Card);
+            }
+        }
+
+        /**
          * Compares a property of two given cards and returns the player who won. If both cards
-         * have the same value, then return stingstack (they are not yet added to stingstack).
+         * have the same value, then return standoff (cards are not yet added to stingstack!).
          * @param property
          * @return
          */
@@ -416,7 +481,7 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
                     return PLAYER2;
                 }
             }
-            return STINGSTACK;
+            return STANDOFF;
         }
 
     /*
@@ -438,5 +503,20 @@ public class GameActivity extends AppCompatActivity implements CardFragment.OnFr
         public void showGameLostDialog(Player p1, Player p2) {
             // TODO: show dialog if player list the game
         }
+
+    /*
+        GETTERS AND SETTERS
+     */
+
+        public int getCurPlayer() {
+            return curPlayer;
+        }
+
+        public void setLastPlayed() {
+            Date date = new Date();
+            this.lastPlayed = new Timestamp(date.getTime());
+        }
     }
+
+
 }
---
+++ FancyQuartettApp/app/src/main/java/de/uulm/mal/fancyquartett/tasks/ToTheEndTask.java
@@ -0,0 +1,77 @@
+package de.uulm.mal.fancyquartett.tasks;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import de.uulm.mal.fancyquartett.activities.GameActivity;
+import de.uulm.mal.fancyquartett.adapters.CardAttrViewAdapter;
+import de.uulm.mal.fancyquartett.data.Card;
+import de.uulm.mal.fancyquartett.data.CardAttribute;
+import de.uulm.mal.fancyquartett.data.Property;
+
+/**
+ * Created by Lukas on 13.01.2016.
+ */
+public class ToTheEndTask extends AsyncTask implements CardAttrViewAdapter.OnCardAttrClickListener{
+
+    private final long TASK_REPEAT_DURATION = 1000;
+
+    private boolean isRunning = false;
+    private boolean playerSelectedCartAttribute= false;
+
+    private Property currentProperty;
+
+    private GameActivity.GameEngine engine;
+    private Context context;
+
+    public ToTheEndTask(Context context, GameActivity.GameEngine engine) {
+        this.context = context;
+        this.engine = engine;
+    }
+
+    @Override
+    protected synchronized Object doInBackground(Object[] params) {
+        while(!isRunning) {
+            // wait some fancy time
+            try {
+
+                wait(TASK_REPEAT_DURATION);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // check curPlayer
+            if( true /*engine.getCurPlayer() == engine.PLAYER1*/) {
+                if(playerSelectedCartAttribute)  {
+                    // lock if statement
+                    playerSelectedCartAttribute = false;
+                    // compare cards an give them to winning player
+                    int playerWonRound = engine.compareCardsProperty(currentProperty);
+                    System.out.println("Player " + playerWonRound + " wins current round!");
+                    engine.handlePlayerCards(playerWonRound);
+                    // check if player won
+                    int playerWonGame = engine.checkPlayerWon();
+                    if(playerWonGame != -1) {
+                        isRunning = true;
+                        System.out.println("Player" + playerWonGame + " wins game!");
+                        // TODO: display won dialog
+                    } else {
+                        // show next card
+                        engine.showNextCard();
+                    }
+                }
+            } else {
+                // TODO: Ai stuff
+            }
+            // change curPlayer
+            // engine.changeCurrentPlayer();
+        }
+        return null;
+    }
+
+    @Override
+    public void onCardAttrClicked(Property property, float value, CardAttribute attribute) {
+        currentProperty = property;
+        playerSelectedCartAttribute = true;
+    }
+}
